e9d4b869c97dc8ac239f82b926805476600c4b28
MD-Muddabir <mohd.muddabiruszama@gmail.com>
2025-09-29 16:52:53 +0530

    in the index.ts file i can add the fucntion and generate the javascript file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var sales = 123456789;
+var course = 'TypeScript';
+var is_published = true;
+var level;
+function greet(name) {
+    return `Hello, ${name}`;
+}
+var message = greet("World");
+console.log(message);
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ let course = 'TypeScript';
 let is_published = true;
 let level;
 
-function render(document: any) {
-    console.log(document);
+function greet(name: string): string {
+    return 'Hello, ${name}!';
 }
+
+const message: string = greet("World");
+console.log(message);
